0f8c5f8917cf1ea70ff4c48bb9d651bbd128be23
Sam Heng On <samhengon@Sams-MacBook-Pro.local>
2018-08-13 17:27:48 +0800

    SamHengOn Keyboard init commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<style>
+.box {
+  height: 100%;
+  border: 1px solid #ccc;
+  height: 45px;
+  padding: 10px;
+  margin: 10px;
+}
+</style>
+<template>
+  <div id="aap">
+    <div class="box">
+       <wc-keyboard 
+        placeholder="询问服务员后输入"
+        label="消费金额"
+        inter="5"
+        decimal="2"
+        v-model="val"
+        @keyboardtouchstart="test"
+        ref="price" />
+    </div>
+    <div class="box">
+       <wc-keyboard 
+        placeholder="ttg2"
+        label="haha2"
+        inter="5"
+        decimal="0"
+        v-model="gg"
+        @keyboardtouchstart="test"
+        ref="qty" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      val: "",
+      gg: ""
+    };
+  },
+  methods: {
+    test(label) {
+      console.log("test emit from keyboardtouchstart..." + label);
+      var priceKeyboardComponent = this.$refs.price;
+      var qtyKeyboardComponent = this.$refs.qty;
+      if (priceKeyboardComponent.label === label)
+      {
+        console.log("emit from keyboardtouchstart: Price Keyboard Clicked!");
+        qtyKeyboardComponent.blur();
+      } else if (qtyKeyboardComponent.label === label)
+      {
+        console.log("emit from keyboardtouchstart: Qty Keyboard Clicked!");
+        priceKeyboardComponent.blur();
+      }
+    }
+  }
+};
+</script>
