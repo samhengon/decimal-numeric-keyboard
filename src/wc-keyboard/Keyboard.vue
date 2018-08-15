876927e8ceec4c4094663309e9ceefba9916a0cf
Sam Heng On <samhengon@gmail.com>
2018-08-15 09:44:26 +0800

    disable double tap zoom on keyboard keys

--- src/wc-keyboard/Keyboard.vue
+++ src/wc-keyboard/Keyboard.vue
@@ -86,26 +86,27 @@
 			</div>
 			<!-- 键盘区域 -->
 			<div class="list">
-				<div class="key" @touchstart="typing('1')">1</div>
-				<div class="key" @touchstart="typing('2')">2</div>
-				<div class="key" @touchstart="typing('3')">3</div>
-				<div class="key" @touchstart="typing('4')">4</div>
-				<div class="key" @touchstart="typing('5')">5</div>
-				<div class="key" @touchstart="typing('6')">6</div>
-				<div class="key" @touchstart="typing('7')">7</div>
-				<div class="key" @touchstart="typing('8')">8</div>
-				<div class="key" @touchstart="typing('9')">9</div>
+				<div class="key" @touchstart.prevent="typing('1')">1</div>
+				<div class="key" @touchstart.prevent="typing('2')">2</div>
+				<div class="key" @touchstart.prevent="typing('3')">3</div>
+				<div class="key" @touchstart.prevent="typing('4')">4</div>
+				<div class="key" @touchstart.prevent="typing('5')">5</div>
+				<div class="key" @touchstart.prevent="typing('6')">6</div>
+				<div class="key" @touchstart.prevent="typing('7')">7</div>
+				<div class="key" @touchstart.prevent="typing('8')">8</div>
+				<div class="key" @touchstart.prevent="typing('9')">9</div>
 				<div class="key dot" 
-					@touchstart="typing('.')" v-if="showDecimal">
+					@touchstart.prevent="typing('.')" v-if="showDecimal">
 					<i class="iconfont icon-dot"></i>
 				</div>
 				<div class="key dot" 
+					@touchstart.prevent
 					 v-if="!showDecimal">
 				</div>
 				<div class="key" 
-					@touchstart="typing('0')">0</div>
+					@touchstart.prevent="typing('0')">0</div>
 				<div class="key"
-					@touchstart="typing('')"
+					@touchstart.prevent="typing('')"
 					>
 					<i class="iconfont icon-keyboard-delete del"></i>
 				</div>
